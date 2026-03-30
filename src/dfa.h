5cfab1868d677de24f5960cfa2bb2364cd43828d
arinazkk <arinaazkaaa@gmail.com>
2026-03-30 17:41:28 +0700

    fix: readPunctuation

--- src/dfa.h
+++ src/dfa.h
@@ -3,7 +3,8 @@
 /**
  * File dfa.h befungsi untuk mendefinisikan semua state keadaan dalam DFA yang dipakai lexer
  */
-enum State{
+enum State
+{
     // State awal
     S0,
 
@@ -20,36 +21,35 @@ enum State{
     S_IDENT,
 
     // operator aritmatika
-    S_PLUS, // '+'
+    S_PLUS,  // '+'
     S_MINUS, // '-'
     S_TIMES, // '*'
-    S_RDIV, // '/'
+    S_RDIV,  // '/'
 
     // operator perbandingan
-    S_EQL1, // baru baca "=" yang pertama -> =
-    S_EQL2, // sudah baca "=" yang kedua ->  ==
-    S_LESS, // <
-    S_LESSEQUAL, // <=
-    S_NOTEQUAL, // !=
-    S_GREATER, // >
+    S_EQL1,         // baru baca "=" yang pertama -> =
+    S_EQL2,         // sudah baca "=" yang kedua ->  ==
+    S_LESS,         // <
+    S_LESSEQUAL,    // <=
+    S_NOTEQUAL,     // !=
+    S_GREATER,      // >
     S_GREATEREQUAL, // >=
 
     // tanda baca & assignment
-    S_COLON, // baru baca ':'
-    S_BECOMES, // sudah baca ':' + '=' -> ':='
-    S_LPAR, // '('
-    S_RPAR, //  ')'
-    S_LBRACK, //  '['
-    S_RBRACK,// ']'
-    S_COMMA, // ','
-    S_SEMI, // ';'
-    S_PERIOD, // '.'
+    S_COLON,     // baru baca ':'
+    S_BECOMES,   // sudah baca ':' + '=' -> ':='
+    S_LPAR,      // '('
+    S_RPAR,      //  ')'
+    S_LBRACK,    //  '['
+    S_RBRACK,    // ']'
+    S_COMMA,     // ','
+    S_SEMICOLON, // ';'
+    S_PERIOD,    // '.'
 
     // komentar
-    S_CMT1, // di dalam komentar { ... }
-    S_CMT2, // di dalam komentar (* ... *)
+    S_CMT1,      // di dalam komentar { ... }
+    S_CMT2,      // di dalam komentar (* ... *)
     S_CMT2_STAR, // sudah baca '*' di dalam (* ... *), nunggu ')'
-
 
     // error
     S_ERROR,
--- src/lexer.cpp
+++ src/lexer.cpp
@@ -54,219 +54,169 @@ Token Lexer::readOperator(char ch)
 {
     string val(1, ch);
 
-<<<<<<< HEAD
     switch (ch)
     {
     case '+':
+        currentState = S_PLUS;
         return Token{"plus", val};
     case '-':
+        currentState = S_MINUS;
         return Token{"minus", val};
     case '*':
+        currentState = S_TIMES;
         return Token{"times", val};
     case '/':
+        currentState = S_RDIV;
         return Token{"rdiv", val};
     case '=':
+        currentState = S_EQL1;
         if (peek() == '=')
         {
             val += advance(); // Consume '=' yang kedua
+            currentState = S_EQL2;
             return Token{"eql", val};
+        }
+        currentState = S_ERROR;
+        return Token{"error", val};
+    case '<':
+        currentState = S_LESS;
+        if (peek() == '>')
+        {
+            val += advance();
+            currentState = S_NOTEQUAL;
+            return Token{"neq", val};
+        }
+        else if (peek() == '=')
+        {
+            val += advance(); // Consume '='
+            currentState = S_LESSEQUAL;
+            return Token{"leq", val};
+        }
+        return Token{"lss", val};
+    case '>':
+        currentState = S_GREATER;
+        if (peek() == '=')
+        {
+            val += advance(); // Consume '='
+            currentState = S_GREATEREQUAL;
+            return Token{"geq", val};
+        }
+        return Token{"gtr", val};
+    default:
+        currentState = S_ERROR;
+        return Token{"error", val};
+    }
+};
+
+Token Lexer::readPunctuation(char ch)
+{
+    string val(1, ch);
+
+    switch (ch)
+    {
+    case ',':
+        currentState = S_COMMA;
+        return Token{"comma", val};
+    case ';':
+        currentState = S_SEMICOLON;
+        return Token{"semicolon", val};
+    case '.':
+        currentState = S_PERIOD;
+        return Token{"period", val};
+    case ':':
+        currentState = S_COLON;
+        if (peek() == '=')
+        {
+            val += advance();
+            currentState = S_BECOMES;
+            return Token{"becomes", val};
         }
         return Token{"colon", val};
     default:
+        currentState = S_ERROR;
         return Token{"error", val};
-        == == == =
-                     switch (ch)
-        {
-        case '+':
-            currentState = S_PLUS;
-            return Token{"plus", val};
-        case '-':
-            currentState = S_MINUS;
-            return Token{"minus", val};
-        case '*':
-            currentState = S_TIMES;
-            return Token{"times", val};
-        case '/':
-            currentState = S_RDIV;
-            return Token{"rdiv", val};
-        case '=':
-            currentState = S_EQL1;
-            if (peek() == '=')
-            {
-                val += advance(); // Consume '=' yang kedua
-                currentState = S_EQL2;
-                return Token{"eql", val};
-            }
-            currentState = S_ERROR;
-            return Token{"error", val};
-        case '<':
-            currentState = S_LESS;
-            if (peek() == '>')
-            {
-                val += advance();
-                currentState = S_NOTEQUAL;
-                return Token{"neq", val};
-            }
-            else if (peek() == '=')
-            {
-                val += advance(); // Consume '='
-                currentState = S_LESSEQUAL;
-                return Token{"leq", val};
-            }
-            return Token{"lss", val};
-        case '>':
-            currentState = S_GREATER;
-            if (peek() == '=')
-            {
-                val += advance(); // Consume '='
-                currentState = S_GREATEREQUAL;
-                return Token{"geq", val};
-            }
-            return Token{"gtr", val};
-        default:
-            currentState = S_ERROR;
-            return Token{"error", val};
->>>>>>> origin/main
-        }
-    };
+    }
+};
 
-    Token Lexer::readPunctuation(char ch)
+string Lexer::classifyKeyword(string val) {};
+
+Token Lexer::readIdentOrKeyword() {};
+
+vector<Token> Lexer::tokenize()
+{
+    vector<Token> tokens;
+
+    while (peek() != '\0')
     {
-        string val(1, ch);
+        currentState = S0;
+        char ch = peek();
 
-        switch (ch)
+        // skip whitespace
+        if (isspace(ch))
         {
-        case ',':
-            return Token{"comma", val};
-        case ';':
-            return Token{"semicolon", val};
-        case '.':
-            return Token{"period", val};
-        case ':':
-            if (peek() == '=')
-            {
-                val += advance(); // Consume '=' yang kedua
-                return Token{"becomes", val};
-            }
-            return Token{"error", val};
-        case '<':
-            if (peek() == '>')
-            {
-                val += advance();
-                return Token{"neq", val};
-            }
-            else if (peek() == '=')
-            {
-                val += advance(); // Consume '='
-                return Token{"leq", val};
-            }
-            return Token{"lss", val};
-        case '>':
-            if (peek() == '=')
-            {
-                val += advance(); // Consume '='
-                return Token{"geq", val};
-            }
-            return Token{"gtr", val};
-        default:
-            return Token{"error", val};
+            advance();
+            continue;
         }
-    };
 
-    string Lexer::classifyKeyword(string val) {};
-    Token Lexer::readIdentOrKeyword() {};
-
-    vector<Token> Lexer::tokenize()
-    {
-        vector<Token> tokens;
-
-        while (peek() != '\0')
+        // read Number
+        if (isdigit(ch))
         {
-            currentState = S0;
-            char ch = peek();
+            tokens.push_back(readNumber());
+        }
 
-            // skip whitespace
-            if (isspace(ch))
-            {
-                advance();
-                continue;
+        // read Identifier(variable name) or Keyword(beginsy, termausuk -> MOD, AND, div, termasuk semua yang pakai string)
+        else if (isalpha(ch))
+        {
+            tokens.push_back(readIdentOrKeyword());
+        }
+
+        // read String
+        else if (ch == '\'')
+        {
+            tokens.push_back(readString());
+        }
+
+        // read Comment
+        else if (ch == '{')
+        {
+            tokens.push_back(readComment(ch));
+        }
+        else if (ch == '(')
+        {
+            if (source[pos + 1] == '*')
+            {              // Cek karakter setelah '('
+                advance(); // Maju untuk '('
+                advance(); // Maju untuk '*'
+                tokens.push_back(readComment('*'));
             }
-
-            // read Number
-            if (isdigit(ch))
+            else
             {
-                tokens.push_back(readNumber());
+                advance(); // Cukup maju untuk '('
+                currentState = S_LPAR;
+                tokens.push_back({"lparent", "("});
             }
+        }
 
-            // read Identifier(variable name) or Keyword(beginsy, termausuk -> MOD, AND, div, termasuk semua yang pakai string)
-            else if (isalpha(ch))
-            {
-                tokens.push_back(readIdentOrKeyword());
-            }
+        else if (ch == '+' || ch == '-' || ch == '*' || ch == '/' ||
+                 ch == '=' || ch == '<' || ch == '>')
+        {
+            advance();
+            tokens.push_back(readOperator(ch));
+        }
 
-            // read String
-            else if (ch == '\'')
-            {
-                tokens.push_back(readString());
-            }
+        else if (ch == ',' || ch == '.' || ch == ';' || ch == ':' ||
+                 ch == ')' || ch == '[' || ch == ']')
+        {
+            advance();
+            tokens.push_back(readPunctuation(ch));
+        }
 
-            // read Comment
-            else if (ch == '{')
-            {
-                tokens.push_back(readComment(ch));
-            }
-<<<<<<< HEAD
-            else if (ch == '(')
-            {
-                advance();
-                if (ch == '*')
-                {
-                    tokens.push_back(readComment(ch));
-                }
-                else
-                {
-                    tokens.push_back({"lparent", "("});
-                }
-                == == == =
-                             else if (ch == '(')
-                {
-                    if (source[pos + 1] == '*')
-                    {              // Cek karakter setelah '('
-                        advance(); // Maju untuk '('
-                        advance(); // Maju untuk '*'
-                        tokens.push_back(readComment('*'));
-                    }
-                    else
-                    {
-                        advance(); // Cukup maju untuk '('
-                        currentState = S_LPAR;
-                        tokens.push_back({"lparent", "("});
-                    }
->>>>>>> origin/main
-                }
-
-                else if (ch == '+' || ch == '-' || ch == '*' || ch == '/' ||
-                         ch == '=' || ch == '<' || ch == '>')
-                {
-                    advance();
-                    tokens.push_back(readOperator(ch));
-                }
-
-                else if (ch == ',' || ch == '.' || ch == ';' || ch == ':' ||
-                         ch == ')' || ch == '[' || ch == ']')
-                {
-                    advance();
-                    tokens.push_back(readPunctuation(ch));
-                }
-
-                // Karakter tidak dikenal
-                else
-                {
-                    tokens.push_back(Token{"Token", string(1, ch)});
-                    advance();
-                }
-
-                return tokens;
-            }
-            return tokens;
-        };
+        // Karakter tidak dikenal
+        else
+        {
+            tokens.push_back(Token{"Token", string(1, ch)});
+            advance();
+        }
+    }
+    return tokens;
+};
